4964c6a175be2d7c7d1f130a766051c7b42862dc
liamjbennett <lbennett@opentable.com>
2014-10-06 16:26:39 +0100

    bringing remaining modules into forge standard

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,38 +1,36 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
-#require 'capybara/rspec'
-#require 'capybara/poltergeist'
+
 
 hosts.each do |host|
-  # Install Puppet
-  install_puppet
+  
+  
+	version = ENV['PUPPET_GEM_VERSION']
+	install_puppet(:version => version)
 end
 
-UNSUPPORTED_PLATFORMS = ['Suse','AIX','Solaris','Windows']
-
-RSpec.configure do |c|
-  # Project root
+Spec.configure do |c|
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
-  # Readable test descriptions
-  c.formatter = :documentation
+	c.formatter = :documentation
 
-  #Capybara.app_host = "http://localhost:4567"
-  #Capybara.register_driver :poltergeist do |app|
-  #  Capybara::Poltergeist::Driver.new(
-  #      app,
-  #      window_size: [1280, 1024]
-  #  )
-  #end
-  #Capybara.default_driver    = :poltergeist
-  #Capybara.javascript_driver = :poltergeist
+	c.before :suite do
+		#The gets around a bug where windows can't validate the cert when using https
+		forge_repo = '--module_repository=http://forge.puppetlabs.com'
 
-  # Configure all nodes in nodeset
-  c.before :suite do
-    # Install module and dependencies
-    puppet_module_install(:source => proj_root, :module_name => 'puppetdb_rundeck')
-    hosts.each do |host|
-      on host, puppet('module', 'install', 'puppetlabs-stdlib'), { :acceptable_exit_codes => [0,1] }
-    end
+		hosts.each do |host|
+			c.host = host
+      
+			
+
+			path = (File.expand_path(File.dirname(__FILE__)+'/../')).split('/')
+			name = path[path.length-1].split('-')[1]
+
+			copy_module_to(host, :source => proj_root, :module_name => name)
+
+      
+			on host, puppet('module','install', forge_repo, "puppetlabs-stdlib"), { :acceptable_exit_codes => [0,1] }
+	    
+	  end
   end
 end
